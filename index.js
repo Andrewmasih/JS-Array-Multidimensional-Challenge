ffb62ef31905ea678b46989b19624d6cb4e1c4a8
Andrew Masih <armasih@outlook.com>
2021-05-30 23:52:19 +0000

    added comments

--- index.js
+++ index.js
@@ -7,15 +7,11 @@
 */
 
 
-/* Requirement:
-
-1/ All varibles must be named correctly and have the correct values.
-
-2/ your 'indexArray' will be tested to ensure that we can use its value to access 15 in the 'multidimensionalArray' 
-
-*/
 
 let multidimensionalArray = [1, 2, 3,[4, 5, 6,[7, 8, 9,[10, 11, 12,[13, 14,[15]]]]]];
+
+// added below
+
 
 let fifteen = multidimensionalArray [3][3][3][3][2][0];
 
